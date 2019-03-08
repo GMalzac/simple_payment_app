b398bbbbb3e1faca266d6c09a01ecfb9c866a5ca
Gregoire Malzac <gregoire.malzac@gmail.com>
2019-03-08 11:31:21 +0000

    Add index and show methods to teddies controller

--- app/controllers/teddies_controller.rb
+++ app/controllers/teddies_controller.rb
@@ -1,2 +1,11 @@
 class TeddiesController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    @teddies = Teddy.all
+  end
+
+  def show
+    @teddy = Teddy.find(params[:id])
+  end
 end
