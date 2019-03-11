fed90522a55034104ff223d60f2ee37830e5b2ee
Gregoire Malzac <gregoire.malzac@gmail.com>
2019-03-11 15:33:16 +0000

    Add stripe purchase button and payment form

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'teddies#index'
   resources :teddies, only: [:index, :show]
-  resources :orders, only: [:show, :create]
+  resources :orders, only: [:show, :create] do
+    resources :payments, only: [:new, :create]
+  end
 end
