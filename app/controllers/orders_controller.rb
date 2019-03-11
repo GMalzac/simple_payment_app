4f64457ebbb975b31672866e5305305500818026
Gregoire Malzac <gregoire.malzac@gmail.com>
2019-03-11 15:19:39 +0000

    Add create method to orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,9 @@
 class OrdersController < ApplicationController
   def create
+    teddy = Teddy.find(params[:teddy_id])
+    order  = Order.create!(teddy_sku: teddy.sku, amount: teddy.price, state: 'pending', user: current_user)
+
+    redirect_to new_order_payment_path(order)
   end
 
   def show
